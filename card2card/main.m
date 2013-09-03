69a8ca1e4e0a09da2740383dc0e21a98cce4c94a
Ivan Alekseev <polecat@nettrash.robox.lan>
2013-09-03 13:48:06 +0400

    Initial Commit

---
+++ card2card/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  card2card
+//
+//  Created by Ivan Alekseev on 03.09.13.
+//  Copyright (c) 2013 OCEAN BANK CJSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
